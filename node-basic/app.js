2b314eee47fcef438e17e8734d3d0b43676b6235
tixuz <hindolmirzo@gmail.com>
2020-08-11 11:49:36 +0500

    lesson 11 of NodeJS

--- node-basic/app.js
+++ node-basic/app.js
@@ -3,6 +3,16 @@
 
 //using destructor to import
 const { sum } = require('./helpers');
+const http = require("http")
+
+const server = http.createServer((req, res) => {
+    res.end("Hello world from node-js");
+});
+
+server.listen(3000);
+
+
+
 //var let const
 const total = sum(10,200);
 console.log("TOTAL: ", total);
