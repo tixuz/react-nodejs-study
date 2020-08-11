478e87be6aea0a84ad4a6cc4fdd79ef79e5d280a
tixuz <hindolmirzo@gmail.com>
2020-08-11 17:06:40 +0500

    lesson 22 of NodeJS with API

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
-
+const morgan = require('morgan');
 
 //routes
-
 // const postRoutes = require('./routes/posts');
 // const getPosts = postRoutes.getPosts;
-
 const { getPosts } = require('./routes/posts');
 
+//middleware
+app.use(morgan("dev"));
+
+const myOwnMiddleware = (req, res, next) => {
+    console.log("I'm a middleware!!");
+    next();
+}
+
+app.use(myOwnMiddleware);
 
 app.get('/', getPosts);
 
