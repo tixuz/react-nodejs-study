c19d42bb0b4b0acdad4be7baa3a0846776bc1d6d
tixuz <hindolmirzo@gmail.com>
2020-08-11 16:33:27 +0500

    lesson 20

---
+++ nodeapi/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello, this World');
+});
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Node JS is listening to the port ${port}`);
+});
