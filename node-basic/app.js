29d80a24e51488f24dcf93303672d3f049822bbd
tixuz <hindolmirzo@gmail.com>
2020-08-11 12:25:18 +0500

    lesson 13 of NodeJS

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,18 +1,28 @@
-// console.log("Hello from node-js");
-// console.log(process);
+// // console.log("Hello from node-js");
+// // console.log(process);
+//
+// //using destructor to import
+// const { sum } = require('./helpers');
+// const http = require("http")
+//
+// const server = http.createServer((req, res) => {
+//     res.end("Hello new world from real node-js");
+// });
+//
+// server.listen(3000);
+//
+//
+//
+// //var let const
+// const total = sum(10,200);
+// console.log("TOTAL: ", total);
 
-//using destructor to import
-const { sum } = require('./helpers');
-const http = require("http")
 
-const server = http.createServer((req, res) => {
-    res.end("Hello new world from real node-js");
-});
+const express = require('express')
+const app = express()
 
-server.listen(3000);
+app.get('/', function (req, res) {
+    res.send('Hey wussap with node js')
+})
 
-
-
-//var let const
-const total = sum(10,200);
-console.log("TOTAL: ", total);
+app.listen(3000)
