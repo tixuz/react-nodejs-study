deb55c5b4760dbf2eb03d670aecbc42e920ffd19
tixuz <hindolmirzo@gmail.com>
2020-08-11 18:46:15 +0500

    lesson 26-27 of NodeJS with API

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -2,10 +2,30 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+// import mongoose
+const mongoose = require('mongoose');
+// load env variables
+const dotenv = require('dotenv');
+dotenv.config()
+
+//db connection
+mongoose.connect(
+    process.env.MONGO_URI,
+    {useNewUrlParser: true,
+            useUnifiedTopology: true}
+)
+    .then(() => console.log('DB Connected'))
+
+
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`)
+});
+
 //routes
 // const postRoutes = require('./routes/posts');
 // const getPosts = postRoutes.getPosts;
-const { getPosts } = require('./routes/posts');
+// const { getPosts } = require('./routes/posts');
+
 const postRoutes = require("./routes/posts")
 
 //middleware
@@ -22,7 +42,7 @@ app.use(morgan("dev"));
 
 app.use('/',postRoutes);
 
-const port = 3000;
+const port =  process.env.PORT; //.env
 app.listen(port, () => {
     console.log(`Node JS is listening to the port ${port}`);
 });
